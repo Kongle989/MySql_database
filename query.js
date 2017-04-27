1174993776b962451b43c7d3222c225c59b46cee
Kong Le <Kong Le>
2017-04-27 16:26:22 -0400

    i think this works

--- query.js
+++ query.js
@@ -1,11 +1,11 @@
 /**
  * Created by Kong on 4/24/2017
  */
-
+// REQUIRE STUFF
 var mysql = require('mysql'),
     inq = require('inquirer'),
     table = require('cli-table'),
-
+// CREATE CONNECTION TO DATABASE
     connection = mysql.createConnection({
         host: 'localhost',
         port: 3306,
@@ -13,22 +13,22 @@ var mysql = require('mysql'),
         password: 'root',
         database: 'bamazon'
     });
-
+// START CONNECTION
 connection.connect(function (up) {
     if (up) throw up;
     console.log('Success ' + connection.threadId);
     buyStuff();
 });
-
+// BUY STUFF
 function buyStuff() {
     let select = 'SELECT * FROM `products`';
-    connection.query(select, function (up, data) {
+    connection.query(select, function (up, data) { // GET LIST TO DISPLAY
         if (up) throw up;
         let stuff = [];
-        for (let i in data) {
-            stuff.push(data[i].product_name);
-            // stuff.push(data[i].item_id + '.' + data[i].product_name + ' $' + data[i].price);
+        for (let i = 0; i < data.length; i++) {
+            stuff.push(data[i].item_id + '.' + data[i].product_name + ' $' + data[i].price);
         }
+        // PICK ITEM AND HOW MANY TO BUY
         inq.prompt([
             {
                 type: 'list',
@@ -50,22 +50,25 @@ function buyStuff() {
             }
 
         ]).then(function (res) {
-            // var id = res.option.split('.');
-            // id = id[0] - 1;
-            // if (data[id].stock_quantity == 0) {
-            //     console.log('\nSorry the item you are trying to purchase is no longer in stock.');
-            //     buyStuff();
-            // }
-            // else if (data[id].stock_quantity < res.quantity) {
-            //     console.log('\nSorry we only have ' + data[id].stock_quantity + ' left in stock. Please try again');
-            //     buyStuff();
-            // }
-            // else {
-            //     connection.query('UPDATE `products` SET ? WHERE ?',
-            //         [{stock_quantity: data[id].stock_quantity - res.quantity},
-            //             {item_id: id + 1}]);
-            //     buyStuff();
-            // }
+            let id = res.option.split('.');
+            id = id[0] - 1;
+            // NONE LEFT IN STOCK
+            if (data[id].stock_quantity === '0') {
+                console.log('\nSorry the item you are trying to purchase is no longer in stock.');
+                buyStuff();
+            }
+            // SOME IN STOCK BUT TRIED TO BUY TOO MANY
+            else if (data[id].stock_quantity < res.quantity) {
+                console.log('\nSorry we only have ' + data[id].stock_quantity + ' left in stock. Please try again');
+                buyStuff();
+            }
+            // HAVE ENOUGH IN STOCK
+            else {
+                connection.query('UPDATE `products` SET ? WHERE ?',
+                    [{stock_quantity: data[id].stock_quantity - res.quantity},
+                        {item_id: id + 1}]);
+                buyStuff();
+            }
         })
     });
 }
